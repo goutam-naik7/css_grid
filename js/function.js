512dae1cd494ecd6bd9467549ddd22058bee633d
goutam-naik7 <naikgautam257@gmail.com>
2021-05-06 20:49:24 +0530

    closure - context

--- js/function.js
+++ js/function.js
@@ -14,3 +14,57 @@ var awFunc = function(first){
 
 var someMoreFunc = awFunc('awe')
 console.log(someMoreFunc('some'));
+
+
+
+// function User(name, age)
+// {
+//     this.name = name;
+//     this.age = age;
+    
+//     this.getProfile = function(){
+//     // outer
+//         console.log(this.name);
+
+//      return function() {
+        
+//         // inner
+//         console.log(this.name);
+//          console.log("Im " + this.name + " and " + this.age + " year old");
+//      }
+//     }
+// }
+
+
+// var user = new User('John', 25);
+// var profile = user.getProfile();
+
+// profile();
+
+
+// using arrow function we can fix inner function  context issue
+
+function User(name, age)
+{
+    this.name = name;
+    this.age = age;
+    
+    this.getProfile = function(){
+    // outer
+        console.log(this.name);
+
+     return () => {
+        
+        // inner
+        console.log(this.name);
+         console.log("Im " + this.name + " and " + this.age + " year old");
+     }
+    }
+}
+
+
+var user = new User('John', 25);
+var profile = user.getProfile();
+
+profile();
+
